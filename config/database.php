9add0a54106e2fb050b4cfb93e33012d1366b5a3
HuynhMinhThu <157386347+HuynhMinhThu@users.noreply.github.com>
2024-05-20 23:24:05 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -8,9 +8,9 @@
   		session_start(); 
  header("Content-type: text/html; charset=utf-8");
  $tenmaychu='us-cluster-east-01.k8s.cleardb.net';
- $tentaikhoan='b643ca59e40a14';
- $pass='46cbc92f';
- $csdl='heroku_0417280b1716f23';
+ $tentaikhoan='b77c845689b204';
+ $pass='eeb5d8d8';
+ $csdl='heroku_519a2f738d646df';
  $conn=mysqli_connect($tenmaychu, $tentaikhoan, $pass, $csdl);
  mysqli_select_db($conn,$csdl);
  mysqli_query($conn,"SET NAMES 'UTF8'");
